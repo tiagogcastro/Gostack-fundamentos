848f4293289db8bb195df1eb6091010a372faaac
Tiaguin061 <tiaguin180@gmail.com>
2021-01-26 12:19:07 -0300

    Upload challenge

---
+++ desafio/src/App.js
@@ -0,0 +1,53 @@
+import React, {useState, useEffect} from "react";
+
+import api from './services/api';
+
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+  useEffect(() => {
+    api.get('/repositories')
+    .then(response => setRepositories(response.data));
+  }, [])
+ 
+  async function handleAddRepository() {
+    const response = await api.post('/repositories', {
+      title: 'Desafio ReactJS',
+      url: 'https://github.com/Tiaguin061',
+      techs: ['NodeJs', 'ReactJS']
+    })
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`/repositories/${id}`);
+
+    const newRepositories = repositories.filter(
+      repository => repository.id !== id
+    );
+    setRepositories(newRepositories);
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
